dc9a3fe6e79e190ec3c6f72f174e8cc63b6cf5a8
Marcin Slezak <marcin.slezak@voucherify.io>
2024-03-21 13:11:18 +0100

    fix tax caslculations

--- packages/voucherify/src/order-to-voucherify-order.ts
+++ packages/voucherify/src/order-to-voucherify-order.ts
@@ -5,12 +5,17 @@ import { toCent } from './to-cent'
 export const orderToVoucherifyOrder = (order: Order): OrdersCreate => {
   return {
     amount: toCent(order.summary.priceBeforeDiscount),
-    items: order.items.map((item) => ({
-      quantity: item.quantity,
-      product_id: item.id,
-      sku_id: item.sku,
-      price: (item.price + item.tax) * 100,
-    })),
+    items: order.items.map((item) => {
+      const subtotal = item.price * item.quantity
+      const tax = subtotal * 0.07
+      return {
+        quantity: item.quantity,
+        product_id: item.id,
+        sku_id: item.sku,
+        price: (item.price + item.tax) * 100,
+        amount: (subtotal + tax) * 100,
+      }
+    }),
   }
 }
 const LOCALISATIONS = ['West Parkland', 'Fas Gas', 'Parkland Calgary']
